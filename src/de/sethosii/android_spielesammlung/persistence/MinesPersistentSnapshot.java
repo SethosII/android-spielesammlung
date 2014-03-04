c21f813c07e3926e8a16fc45d5ba2dc9869933e6
SethosII <paul.jaehne@gmx.de>
2014-03-04 18:20:49 +0100

    Javadoc Kommentare in Mines hinzugefügt

--- src/de/sethosii/android_spielesammlung/persistence/MinesPersistentSnapshot.java
+++ src/de/sethosii/android_spielesammlung/persistence/MinesPersistentSnapshot.java
@@ -4,10 +4,13 @@ import de.sethosii.android_spielesammlung.EnumGameState;
 
 public class MinesPersistentSnapshot extends PersistentSnapshotBase {
 
-	public boolean mark;
+	//** save gamestate */
 	public EnumGameState end;
+	//** save remaining mines */
 	public int mineCount;
+	//** save solution */
 	public String[][] solution;
+	//** save user view */
 	public String[][] view;
 	
 }
